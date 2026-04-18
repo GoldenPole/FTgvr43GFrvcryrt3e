1ad3dc86e8f71f64de0e38720f4073418c4f0e69
GoldenPole <gelbogdan44@gmail.com>
2026-04-19 00:17:35 +0300

    Add files via upload

--- haivka-works.js
+++ haivka-works.js
@@ -10,6 +10,7 @@ const haivkaWorks = [
     emoji: "",
     images: ["https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_9941.webp", "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_4035.webp", "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_4019.webp" , "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_4004.webp", "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_4003.webp"],
     price: 420,
+    stock: 1,
     badge: null,
     materials: [],
     desc: ""
@@ -22,6 +23,7 @@ const haivkaWorks = [
     emoji: "",
     images: ["https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_9952.webp" , "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_3704.webp", "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_3707.webp"],
     price: 420,
+    stock: 0,
     badge: null,
     materials: [],
     desc: ""
@@ -38,6 +40,41 @@ const haivkaWorks = [
         "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/lyalya.webp"
     ],
     price: 90,
+    stock: 1,
+    badge: null,
+    materials: [],
+    desc: ""
+  },
+  {
+    id: 4,
+    name: "Білий горсик",
+    category: "Horsyk",
+    categoryLabel: "Горсик",
+    emoji: "",
+    images: [
+      "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_2459.webp",
+      "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_2479.webp",
+      "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_2492.webp"
+    ],
+    price: 420,
+    stock: 1,
+    badge: "new",
+    badgeLabel: "Нове",
+    materials: [],
+    desc: ""
+  },
+  {
+    id: 5,
+    name: "Білий подовжений горсик",
+    category: "Horsyk",
+    categoryLabel: "Горсик",
+    emoji: "",
+    images: [
+      "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_4095.webp",
+      "https://qtcvngzcbjpnosriaxga.supabase.co/storage/v1/object/public/haivka/IMG_4116.webp"
+    ],
+    price: 420,
+    stock: 1,
     badge: "new",
     badgeLabel: "Нове",
     materials: [],
